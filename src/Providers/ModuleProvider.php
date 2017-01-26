ae3ed677d9e479f323f94b362c830ac134807758
Tedozi Manson <duyphan.developer@gmail.com>
2017-01-26 13:36:08 +0700

    Add composer.phar

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -37,6 +37,7 @@ class ModuleProvider extends ServiceProvider
             __DIR__ . '/../../resources/assets' => resource_path('assets'),
         ], 'webed-assets');
         $this->publishes([
+            __DIR__ . '/../../resources/root' => base_path(),
             __DIR__ . '/../../resources/public' => public_path(),
         ], 'webed-public-assets');
     }
