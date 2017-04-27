292535dcd3180300561a5b3c9d4ad06d2c9bbd2f
Tedozi Manson <duyphan.developer@gmail.com>
2017-04-27 16:57:35 +0700

    Refactor get image

--- resources/views/admin/_components/select-image-box.blade.php
+++ resources/views/admin/_components/select-image-box.blade.php
@@ -7,7 +7,7 @@
  */
 $label = isset($label) ? $label : trans('webed-core::base.form.choose_image');
 $value = isset($value) ? $value : '';
-$thumbnail = isset($thumbnail) ? $thumbnail : '/admin/images/no-image.png';
+$thumbnail = isset($thumbnail) ? $thumbnail : asset('/admin/images/no-image.png');
 ?>
 <div class="select-media-box">
     <button type="button" class="btn blue show-add-media-popup">
@@ -15,7 +15,7 @@ $thumbnail = isset($thumbnail) ? $thumbnail : '/admin/images/no-image.png';
     </button>
     <div class="clearfix"></div>
     <a title="" class="show-add-media-popup" href="javascript:;">
-        <img src="{{ $value ?: $thumbnail }}" alt="{{ $value }}" class="img-responsive">
+        <img src="{{ asset($value ?: $thumbnail) }}" alt="{{ $value }}" class="img-responsive">
     </a>
     <input type="hidden" name="{{ $name }}" value="{{ $value or '' }}" class="input-file">
     <a title="" class="remove-image" href="javascript:;">
