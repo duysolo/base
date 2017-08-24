1c3ff7125a5153dc93a2b3a59e1eed7477669e04
Tedozi Manson <duyphan.developer@gmail.com>
2017-08-24 09:08:25 +0700

    Update master view for auth actions

--- resources/views/admin/_master.blade.php
+++ resources/views/admin/_master.blade.php
@@ -49,71 +49,98 @@
 </div>
 <!-- Loading state -->
 
-<div class="wrapper">
-    {{--BEGIN Header--}}
-    @include('webed-core::admin._partials.header')
-    {{--END Header--}}
+@if(!isset($isAuthPages) || !$isAuthPages)
+    <div class="wrapper">
+        {{--BEGIN Header--}}
+        @include('webed-core::admin._partials.header')
+        {{--END Header--}}
 
-    {{--BEGIN Sidebar--}}
-    @include('webed-core::admin._partials.sidebar')
-    {{--END Sidebar--}}
+        {{--BEGIN Sidebar--}}
+        @include('webed-core::admin._partials.sidebar')
+        {{--END Sidebar--}}
 
-    <div class="content-wrapper">
-        <section class="content-header">
-            {{--BEGIN Page title--}}
-            @include('webed-core::admin._partials.page-title')
-            {{--END Page title--}}
-            {{--BEGIN Breadcrumbs--}}
-            @include('webed-core::admin._partials.breadcrumbs')
-            {{--END Breadcrumbs--}}
-        </section>
+        <div class="content-wrapper">
+            <section class="content-header">
+                {{--BEGIN Page title--}}
+                @include('webed-core::admin._partials.page-title')
+                {{--END Page title--}}
+                {{--BEGIN Breadcrumbs--}}
+                @include('webed-core::admin._partials.breadcrumbs')
+                {{--END Breadcrumbs--}}
+            </section>
 
-        <section class="content">
-            {{--BEGIN Flash messages--}}
-            @include('webed-core::admin._partials.flash-messages')
-            {{--END Flash messages--}}
+            <section class="content">
+                {{--BEGIN Flash messages--}}
+                @include('webed-core::admin._partials.flash-messages')
+                {{--END Flash messages--}}
 
-            {{--BEGIN Content--}}
-            @yield('content')
-            {{--END Content--}}
-        </section>
+                {{--BEGIN Content--}}
+                @yield('content')
+                {{--END Content--}}
+            </section>
+        </div>
+
+        {{--BEGIN Footer--}}
+        @include('webed-core::admin._partials.footer')
+        {{--END Footer--}}
+
+        {{--BEGIN control sidebar--}}
+        @include('webed-core::admin._partials.control-sidebar')
+        {{--END control sidebar--}}
     </div>
-
-    {{--BEGIN Footer--}}
-    @include('webed-core::admin._partials.footer')
-    {{--END Footer--}}
-
-    {{--BEGIN control sidebar--}}
-    @include('webed-core::admin._partials.control-sidebar')
-    {{--END control sidebar--}}
-</div>
+@else
+    <div class="auth-actions-bar">
+        <div class="text-right">
+            <div class="dropdown">
+                <a href="javascript:;"
+                   class="dropdown-toggle"
+                   data-toggle="dropdown"
+                   data-hover="dropdown"
+                   data-close-others="true">
+                    {{ trans('webed-core::languages.' . dashboard_language()->getDashboardLanguage()) }}
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    @foreach(config('webed.languages', []) as $slug => $language)
+                        <li class="{{ $slug == dashboard_language()->getDashboardLanguage() ? 'active' : '' }}">
+                            <a href="{{ route('admin::dashboard-language.get', [$slug]) }}">
+                                {{ trans('webed-core::languages.' . $slug) }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+@endif
 
 {{--Modals--}}
 @include('webed-core::admin._partials.modals')
 
 <!--[if lt IE 9]>
-<script src="admin/plugins/respond.min.js"></script>
-<script src="admin/plugins/excanvas.min.js"></script>
-<![endif]-->
+    <script src="admin/plugins/respond.min.js"></script>
+    <script src="admin/plugins/excanvas.min.js"></script>
+    <![endif]-->
 
-{{--BEGIN plugins--}}
-<script src="{{ asset('admin/theme/lte/js/app.js') }}"></script>
-<script src="{{ asset('admin/js/webed-core.js') }}"></script>
-<script src="{{ asset('admin/theme/lte/js/demo.js') }}"></script>
-{!! assets_management()->renderScripts('bottom') !!}
-{{--END plugins--}}
+    {{--BEGIN plugins--}}
+    <script src="{{ asset('admin/theme/lte/js/app.js') }}"></script>
+    <script src="{{ asset('admin/js/webed-core.js') }}"></script>
+    <script src="{{ asset('admin/theme/lte/js/demo.js') }}"></script>
+    {!! assets_management()->renderScripts('bottom') !!}
+    {{--END plugins--}}
 
-@php do_action(BASE_ACTION_FOOTER_JS) @endphp
+    @php do_action(BASE_ACTION_FOOTER_JS) @endphp
 
-<script src="{{ asset('admin/js/script.js') }}"></script>
+    <script src="{{ asset('admin/js/script.js') }}"></script>
 
-@stack('js')
+    @stack('js')
 
-@yield('js')
+    @yield('js')
 
-@stack('js-init')
+    @stack('js-init')
 
-@yield('js-init')
+    @yield('js-init')
 
 </body>
 
