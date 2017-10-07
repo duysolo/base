040f435539d60b7659a9525fc30658030ab9beee
Tedozi Manson <duyphan.developer@gmail.com>
2017-10-07 11:10:43 +0700

    Refactor

---
+++ src/Hook/StoreSettingsHook.php
@@ -0,0 +1,37 @@
+<?php namespace WebEd\Base\Hook;
+
+use Illuminate\Http\Request;
+
+class StoreSettingsHook
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    public function __construct()
+    {
+        $this->request = request();
+    }
+
+    /**
+     * @param $data
+     * @param $screenName
+     * @param null $action
+     * @return mixed
+     */
+    public function execute($data, $screenName, $action = null)
+    {
+        if ($screenName !== WEBED_SETTINGS) {
+            dd($screenName);
+            return $data;
+        }
+
+        if ($this->request->input('_tab') === 'advanced' && $action === 'edit.post') {
+            $data['construction_mode'] = (int)($this->request->has('construction_mode'));
+            $data['show_admin_bar'] = (int)($this->request->has('show_admin_bar'));
+        }
+
+        return $data;
+    }
+}
--- src/Providers/HookServiceProvider.php
+++ src/Providers/HookServiceProvider.php
@@ -1,14 +1,10 @@
 <?php namespace WebEd\Base\Providers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use WebEd\Base\Hook\StoreSettingsHook;
 
 class HookServiceProvider extends ServiceProvider
 {
-    /**
-     * @var Request
-     */
-    protected $request;
     /**
      * Bootstrap any application services.
      *
@@ -16,39 +12,6 @@ class HookServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->booted(function () {
-            $this->request = request();
-
-            $this->booted();
-        });
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
-     * Callback when app booted
-     *
-     * @return void
-     */
-    protected function booted()
-    {
-        add_filter(BASE_FILTER_BEFORE_UPDATE, function ($data, $screenName, $action = null) {
-            if ($screenName !== WEBED_SETTINGS) {
-                return $data;
-            }
-            if($this->request->input('_tab') === 'advanced') {
-                $data['construction_mode'] = (int)($this->request->has('construction_mode'));
-                $data['show_admin_bar'] = (int)($this->request->has('show_admin_bar'));
-            }
-            return $data;
-        });
+        add_filter(BASE_FILTER_BEFORE_UPDATE, [StoreSettingsHook::class, 'execute'], 30);
     }
 }
